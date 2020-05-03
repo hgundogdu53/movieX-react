741c0cad9963d668c19d19dc8dbed1a7c440c6e2
Huseyin Gundogdu <hgundogdu53@gmail.com>
2020-05-03 15:16:35 -0700

    title changed

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import Movie from '../Movie/Movie';
-import '../../index.css';
+import React from "react";
+import Movie from "../Movie/Movie";
+import "../../index.css";
 
 function MovieList({ movies }) {
-    return (
-        <div className="movie-list">
-
-            {movies && movies.map(movie => {
-                return (
-                    <Movie
-                        id={movie.id}
-                        key={movie.id}
-                        title={movie.original_title}
-                        posterPath={"http://image.tmdb.org/t/p/w400" + movie.poster_path}
-                    />
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="movie-list">
+      {movies &&
+        movies.map((movie) => {
+          return (
+            <Movie
+              id={movie.id}
+              key={movie.id}
+              title={movie.title}
+              posterPath={"http://image.tmdb.org/t/p/w400" + movie.poster_path}
+            />
+          );
+        })}
+    </div>
+  );
 }
 
 export default MovieList;
